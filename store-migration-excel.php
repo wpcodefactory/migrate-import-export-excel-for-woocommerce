97851bb2e230d42b879467a2fddf794ce9c2bc2a
algoritmika <info@algoritmika.com>
2025-05-16 17:24:04 +0300

    Cleanup

--- store-migration-excel.php
+++ store-migration-excel.php
@@ -24,7 +24,7 @@
 
 defined( 'ABSPATH' ) || exit;
 
-defined( 'WPFACTORY_WC_PROCM_VERSION' ) || define( 'WPFACTORY_WC_PROCM_VERSION', '3.0.0-dev-20250516-1411' );
+defined( 'WPFACTORY_WC_PROCM_VERSION' ) || define( 'WPFACTORY_WC_PROCM_VERSION', '3.0.0-dev-20250516-1423' );
 
 defined( 'WPFACTORY_WC_PROCM_FILE' ) || define( 'WPFACTORY_WC_PROCM_FILE', __FILE__ );
 
@@ -90,58 +90,59 @@ class StoreMigrationWooCommerce extends StoreMigrationWooCommerceInit {
 		register_activation_hook( __FILE__, array( $this, 'onActivation' ) );
 		register_deactivation_hook( __FILE__, array( $this, 'onDeactivation' ) );
 
-			$products = new StoreMigrationWooCommerce_Products();
-			$users    = new StoreMigrationWooCommerce_Customers();
-			$orders   = new StoreMigrationWooCommerce_Orders();
-			$coupons  = new StoreMigrationWooCommerce_Coupons();
+		$products = new StoreMigrationWooCommerce_Products();
+		$users    = new StoreMigrationWooCommerce_Customers();
+		$orders   = new StoreMigrationWooCommerce_Orders();
+		$coupons  = new StoreMigrationWooCommerce_Coupons();
 
-			add_action( 'wp_ajax_nopriv_import_process', array( $products, 'import_process' ) );
-			add_action( 'wp_ajax_import_process', array( $products, 'import_process' ) );
+		add_action( 'wp_ajax_nopriv_import_process', array( $products, 'import_process' ) );
+		add_action( 'wp_ajax_import_process', array( $products, 'import_process' ) );
 
-			add_action( 'wp_ajax_export_process', array( $products, 'export_process' ) );
-			add_action( 'wp_ajax_nopriv_export_process', array( $products, 'export_process' ) );
+		add_action( 'wp_ajax_export_process', array( $products, 'export_process' ) );
+		add_action( 'wp_ajax_nopriv_export_process', array( $products, 'export_process' ) );
 
-			add_action( 'wp_ajax_exportUsers_process', array( $users, 'exportUsers_process' ) );
-			add_action( 'wp_ajax_nopriv_exportUsers_process', array( $users, 'exportUsers_process' ) );
+		add_action( 'wp_ajax_exportUsers_process', array( $users, 'exportUsers_process' ) );
+		add_action( 'wp_ajax_nopriv_exportUsers_process', array( $users, 'exportUsers_process' ) );
 
-			add_action( 'wp_ajax_exportCoupons_process', array( $coupons, 'exportCoupons_process' ) );
-			add_action( 'wp_ajax_nopriv_exportCoupons_process', array( $coupons, 'exportCoupons_process' ) );
+		add_action( 'wp_ajax_exportCoupons_process', array( $coupons, 'exportCoupons_process' ) );
+		add_action( 'wp_ajax_nopriv_exportCoupons_process', array( $coupons, 'exportCoupons_process' ) );
 
-			add_action( 'wp_ajax_exportOrders_process', array( $orders, 'exportOrders_process' ) );
-			add_action( 'wp_ajax_nopriv_exportOrders_process', array( $orders, 'exportOrders_process' ) );
+		add_action( 'wp_ajax_exportOrders_process', array( $orders, 'exportOrders_process' ) );
+		add_action( 'wp_ajax_nopriv_exportOrders_process', array( $orders, 'exportOrders_process' ) );
 
-			add_action( 'wp_ajax_importCustomers_process', array( $users, 'importCustomers_process' ) );
-			add_action( 'wp_ajax_nopriv_importCustomers_process', array( $users, 'importCustomers_process' ) );
+		add_action( 'wp_ajax_importCustomers_process', array( $users, 'importCustomers_process' ) );
+		add_action( 'wp_ajax_nopriv_importCustomers_process', array( $users, 'importCustomers_process' ) );
 
-			add_action( 'wp_ajax_importCoupons_process', array( $coupons, 'importCoupons_process' ) );
-			add_action( 'wp_ajax_nopriv_importCoupons_process', array( $coupons, 'importCoupons_process' ) );
+		add_action( 'wp_ajax_importCoupons_process', array( $coupons, 'importCoupons_process' ) );
+		add_action( 'wp_ajax_nopriv_importCoupons_process', array( $coupons, 'importCoupons_process' ) );
 
-			add_action( 'wp_ajax_importOrders_process', array( $orders, 'importOrders_process' ) );
-			add_action( 'wp_ajax_nopriv_importOrders_process', array( $orders, 'importOrders_process' ) );
+		add_action( 'wp_ajax_importOrders_process', array( $orders, 'importOrders_process' ) );
+		add_action( 'wp_ajax_nopriv_importOrders_process', array( $orders, 'importOrders_process' ) );
 
-			add_filter( 'woocommerce_order_data_store_cpt_get_orders_query', array( $this, 'handle_custom_query_var' ), 10, 2 );
+		add_filter( 'woocommerce_order_data_store_cpt_get_orders_query', array( $this, 'handle_custom_query_var' ), 10, 2 );
 
-			// deactivation survey
+		// deactivation survey
 
-			include plugin_dir_path( __FILE__ ) . '/lib/codecabin/plugin-deactivation-survey/deactivate-feedback-form.php';
-			add_filter(
-				'codecabin_deactivate_feedback_form_plugins',
-				function ( $plugins ) {
+		include plugin_dir_path( __FILE__ ) . '/lib/codecabin/plugin-deactivation-survey/deactivate-feedback-form.php';
+		add_filter(
+			'codecabin_deactivate_feedback_form_plugins',
+			function ( $plugins ) {
 
-					$plugins[] = (object) array(
-						'slug'    => 'store-migration-products-orders-import-export-with-excel',
-						'version' => '2.0',
-					);
+				$plugins[] = (object) array(
+					'slug'    => 'store-migration-products-orders-import-export-with-excel',
+					'version' => '2.0',
+				);
 
-					return $plugins;
-				}
-			);
+				return $plugins;
+			}
+		);
 
-			register_activation_hook( __FILE__, array( $this, 'notification_hook' ) );
+		register_activation_hook( __FILE__, array( $this, 'notification_hook' ) );
 
-			add_action( 'admin_notices', array( $this, 'notification' ) );
-			add_action( 'wp_ajax_nopriv_push_not', array( $this, 'push_not' ) );
-			add_action( 'wp_ajax_push_not', array( $this, 'push_not' ) );
+		add_action( 'admin_notices', array( $this, 'notification' ) );
+		add_action( 'wp_ajax_nopriv_push_not', array( $this, 'push_not' ) );
+		add_action( 'wp_ajax_push_not', array( $this, 'push_not' ) );
+
 	}
 
 	public function notification() {
@@ -154,27 +155,27 @@ class StoreMigrationWooCommerce extends StoreMigrationWooCommerceInit {
 		/* Check transient, if available display notice */
 		if ( get_transient( $this->plugin . '_notification' ) ) {
 			?>
-				<div class="updated notice  eshopMigrationWooCommerce_notification">
-					<a href="#" class='dismiss' style='float:right;padding:4px' >close</a>
-					<h3><?php esc_html_e( 'Add your Email below & get ', 'store-migration-products-orders-import-export-with-excel' ); ?><strong style='color:#00a32a'>10%</strong><?php esc_html_e( ' in our PRO plugins! ', 'store-migration-products-orders-import-export-with-excel' ); ?></h3>
-					<form method='post' id='eshopMigrationWooCommerce_signup'>
-						<p>
-						<input required type='email' name='woopei_email' />
-						<input required type='hidden' name='product' value='3525' />
-						<input type='submit' class='button button-primary' name='submit' value='<?php esc_html_e( 'Sign up', 'store-migration-products-orders-import-export-with-excel' ); ?>' />
-						<i><?php esc_html_e( 'By adding your email you will be able to use your email as coupon to a future purchase at ', 'store-migration-products-orders-import-export-with-excel' ); ?><a href='https://extend-wp.com' target='_blank' >extend-wp.com</a></i>
-						</p>
+			<div class="updated notice  eshopMigrationWooCommerce_notification">
+				<a href="#" class='dismiss' style='float:right;padding:4px' >close</a>
+				<h3><?php esc_html_e( 'Add your Email below & get ', 'store-migration-products-orders-import-export-with-excel' ); ?><strong style='color:#00a32a'>10%</strong><?php esc_html_e( ' in our PRO plugins! ', 'store-migration-products-orders-import-export-with-excel' ); ?></h3>
+				<form method='post' id='eshopMigrationWooCommerce_signup'>
+					<p>
+					<input required type='email' name='woopei_email' />
+					<input required type='hidden' name='product' value='3525' />
+					<input type='submit' class='button button-primary' name='submit' value='<?php esc_html_e( 'Sign up', 'store-migration-products-orders-import-export-with-excel' ); ?>' />
+					<i><?php esc_html_e( 'By adding your email you will be able to use your email as coupon to a future purchase at ', 'store-migration-products-orders-import-export-with-excel' ); ?><a href='https://extend-wp.com' target='_blank' >extend-wp.com</a></i>
+					</p>
 
-					</form>
-				</div>
-				<?php
+				</form>
+			</div>
+			<?php
 		}
 	}
 
 	public function push_not() {
-
 		delete_transient( $this->plugin . '_notification' );
 	}
+
 	public function notification_hook() {
 		set_transient( $this->plugin . '_notification', true );
 	}
@@ -183,7 +184,6 @@ class StoreMigrationWooCommerce extends StoreMigrationWooCommerceInit {
 		if ( isset( $query_vars['s'] ) && ! empty( $query_vars['s'] ) ) {
 			$query['s'] = esc_attr( $query_vars['s'] );
 		}
-
 		return $query;
 	}
 
@@ -194,7 +194,7 @@ class StoreMigrationWooCommerce extends StoreMigrationWooCommerceInit {
 	}
 
 	public function print_scripts() {
-				// if want to print some inline script
+		// if want to print some inline script
 	}
 
 	public function BackEndScripts( $hook ) {
@@ -246,41 +246,41 @@ class StoreMigrationWooCommerce extends StoreMigrationWooCommerceInit {
 	}
 
 	public function proModal() {
-
 		?>
-			<div id="<?php print esc_html( $this->plugin ) . 'Modal'; ?>" style='display:none;'>
-				<!-- Modal content -->
-				<div class="modal-content">
-				<div class='<?php print esc_html( $this->plugin ); ?>clearfix'><span class="close">&times;</span></div>
-				<div class='<?php print esc_html( $this->plugin ); ?>clearfix'>
-					<div class='<?php print esc_html( $this->plugin ); ?>columns2'>
-						<center>
-							<img style='width:90%' src='<?php echo esc_url( plugins_url( 'images/' . esc_html( $this->slug ) . '-pro.png', __FILE__ ) ); ?>' style='width:100%' />
-						</center>
-					</div>
-
-					<div class='<?php print esc_html( $this->plugin ); ?>columns2'>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'Import Variable, Affiliate, Subscription Products with excel', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'WPML support for Product Translations Import ', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'ACF & YOAST SEO META support for Import in products', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'export Variable, Affiliate, Subscription Products to excel', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'Products custom Fields Support', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'Bulk import taxonomy terms for products  with excel', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'import Product Reviews  with excel', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'export Product Reviews  to excel', 'store-migration-products-orders-import-export-with-excel' ); ?></strong></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'import Orders with variable products', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'import Orders & apply Coupons', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'import/export WooCommerce Subscriptions', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'create Customer during Order import', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( 'Customers custom fields Support', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p><i class='fa fa-check'></i> <?php esc_html_e( '.. and a lot more!', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
-						<p class='bottomToUp'><center><a target='_blank' class='proUrl' href='<?php print esc_url( $this->proUrl ); ?>'><?php esc_html_e( 'GET IT HERE', 'store-migration-products-orders-import-export-with-excel' ); ?></a></center></p>
-					</div>
+		<div id="<?php print esc_html( $this->plugin ) . 'Modal'; ?>" style='display:none;'>
+			<!-- Modal content -->
+			<div class="modal-content">
+			<div class='<?php print esc_html( $this->plugin ); ?>clearfix'><span class="close">&times;</span></div>
+			<div class='<?php print esc_html( $this->plugin ); ?>clearfix'>
+				<div class='<?php print esc_html( $this->plugin ); ?>columns2'>
+					<center>
+						<img style='width:90%' src='<?php echo esc_url( plugins_url( 'images/' . esc_html( $this->slug ) . '-pro.png', __FILE__ ) ); ?>' style='width:100%' />
+					</center>
 				</div>
+
+				<div class='<?php print esc_html( $this->plugin ); ?>columns2'>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'Import Variable, Affiliate, Subscription Products with excel', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'WPML support for Product Translations Import ', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'ACF & YOAST SEO META support for Import in products', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'export Variable, Affiliate, Subscription Products to excel', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'Products custom Fields Support', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'Bulk import taxonomy terms for products  with excel', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'import Product Reviews  with excel', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'export Product Reviews  to excel', 'store-migration-products-orders-import-export-with-excel' ); ?></strong></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'import Orders with variable products', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'import Orders & apply Coupons', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'import/export WooCommerce Subscriptions', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'create Customer during Order import', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( 'Customers custom fields Support', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p><i class='fa fa-check'></i> <?php esc_html_e( '.. and a lot more!', 'store-migration-products-orders-import-export-with-excel' ); ?></p>
+					<p class='bottomToUp'><center><a target='_blank' class='proUrl' href='<?php print esc_url( $this->proUrl ); ?>'><?php esc_html_e( 'GET IT HERE', 'store-migration-products-orders-import-export-with-excel' ); ?></a></center></p>
 				</div>
 			</div>
-			<?php
+			</div>
+		</div>
+		<?php
 	}
+
 }
 
 $instantiate = new StoreMigrationWooCommerce();
