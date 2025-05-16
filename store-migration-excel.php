2832bb3812a4001f0830dd9cea2435b5dd2ae6ff
algoritmika <info@algoritmika.com>
2025-05-16 18:05:24 +0300

    Cleanup

--- store-migration-excel.php
+++ store-migration-excel.php
@@ -69,9 +69,6 @@ class StoreMigrationWooCommerce extends StoreMigrationWooCommerceInit {
 	public $menuPosition = '50';
 	public $description  = 'Migrate -import and/or export - your Products, Reviews, Customers, Orders to WooCommerce with Excel';
 
-	public $localizeBackend;
-	public $localizeFrontend;
-
 	/**
 	 * Constructor.
 	 *
@@ -205,35 +202,35 @@ class StoreMigrationWooCommerce extends StoreMigrationWooCommerceInit {
 
 		$screen = get_current_screen();
 
-		wp_enqueue_style( esc_html( $this->plugin ) . 'adminCss', plugins_url( '/css/backend.css?v=olu', __FILE__ ) );
+		wp_enqueue_style( $this->plugin . 'adminCss', plugins_url( '/css/backend.css?v=olu', __FILE__ ) );
 
 		wp_enqueue_script( 'jquery' );
 		wp_enqueue_media();
 		wp_enqueue_style( 'wp-color-picker' );
 		wp_enqueue_script( 'jquery-ui-tabs' );
-		wp_enqueue_style( esc_html( $this->plugin ) . 'ui_style', plugins_url( '/css/jquery-ui.css', __FILE__ ) );
-		wp_enqueue_script( esc_html( $this->plugin ) . 'xlsx', plugins_url( '/js/xlsx.js', __FILE__ ), array( 'jquery' ), null, true );
+		wp_enqueue_style( $this->plugin . 'ui_style', plugins_url( '/css/jquery-ui.css', __FILE__ ) );
+		wp_enqueue_script( $this->plugin . 'xlsx', plugins_url( '/js/xlsx.js', __FILE__ ), array( 'jquery' ), null, true );
 
-		wp_enqueue_script( esc_html( $this->plugin ) . 'filesaver', plugins_url( '/js/filesaver.js', __FILE__ ), array( 'jquery' ), null, true );
+		wp_enqueue_script( $this->plugin . 'filesaver', plugins_url( '/js/filesaver.js', __FILE__ ), array( 'jquery' ), null, true );
 
-		wp_enqueue_script( esc_html( $this->plugin ) . 'tableexport', plugins_url( '/js/tableexport.js', __FILE__ ), array( 'jquery' ), null, true );
+		wp_enqueue_script( $this->plugin . 'tableexport', plugins_url( '/js/tableexport.js', __FILE__ ), array( 'jquery' ), null, true );
 
-		if ( ! wp_script_is( esc_html( $this->plugin ) . '_fa', 'enqueued' ) ) {
-			wp_enqueue_style( esc_html( $this->plugin ) . '_fa', plugins_url( '/css/font-awesome.min.css', __FILE__ ) );
+		if ( ! wp_script_is( $this->plugin . '_fa', 'enqueued' ) ) {
+			wp_enqueue_style( $this->plugin . '_fa', plugins_url( '/css/font-awesome.min.css', __FILE__ ) );
 		}
 
-		wp_enqueue_script( esc_html( $this->plugin ) . 'adminJs', plugins_url( '/js/backend.js?v=olu', __FILE__ ), array( 'jquery', 'wp-color-picker', 'jquery-ui-tabs', 'jquery-ui-draggable', 'jquery-ui-droppable', 'jquery-ui-accordion' ), null, true );
+		wp_enqueue_script( $this->plugin . 'adminJs', plugins_url( '/js/backend.js?v=olu', __FILE__ ), array( 'jquery', 'wp-color-picker', 'jquery-ui-tabs', 'jquery-ui-draggable', 'jquery-ui-droppable', 'jquery-ui-accordion' ), null, true );
 
-		$this->localizeBackend = array(
+		$localize = array(
 			'RestRoot'       => esc_url_raw( rest_url() ),
 			'plugin_url'     => plugins_url( '', __FILE__ ),
 			'siteUrl'        => site_url(),
 			'ajax_url'       => admin_url( 'admin-ajax.php' ),
 			'nonce'          => wp_create_nonce( 'wp_rest' ),
-			'plugin_wrapper' => esc_html( $this->plugin ),
+			'plugin_wrapper' => $this->plugin,
 			'exportfile'     => plugins_url( '/js/tableexport.js', __FILE__ ),
 		);
-		wp_localize_script( esc_html( $this->plugin ) . 'adminJs', esc_html( $this->plugin ), $this->localizeBackend );
+		wp_localize_script( $this->plugin . 'adminJs', $this->plugin, $localize );
 
 	}
 
